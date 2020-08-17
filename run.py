a7e8aae2386914d3ab0c86ee61ac3b5bee96a471
sagun98 <sagunmaharjann@gmail.com>
2020-08-17 14:49:01 -0400

    Document generation working

--- run.py
+++ run.py
@@ -4,31 +4,39 @@ from anadama2.tracked import TrackedExecutable
 workflow = Workflow(version="0.0.1", description="Analysis Template")
 # Setting the custom arguments for run.py
 workflow.add_argument(name="lines", desc="Number of lines to trim [default: 10]", default=10)
-workflow.add_argument(name="sample-metadata", desc="Sample metadata for R analysis [default: input/data/metadata.tsv]", default="input/demo_r/input/metadata.tsv")
+workflow.add_argument(name="sample-metadata", desc="Sample metadata for R analysis [default: input/metadata.tsv]", default="input/metadata.tsv")
 args = workflow.parse_args()
 
 # sample python analysis module task0 - input/src/trim.py
 workflow.add_task(
-    "[depends[0]] --input [depends[1]] --output [targets[0]] --lines [args[0]]", 
-    depends=[TrackedExecutable("trim.py"),args.input],
-    targets=[args.output+"example/output_table.tsv"],
+    "python src/trim.py --input input/data.tsv --output [targets[0]] --lines [args[0]]", 
+    depends=[args.input],
+    targets=args.output+"output_table.tsv",
+    args=[args.lines])
+
+# sample python visualization module task0 - input/src/trim.py
+workflow.add_task(
+    "python src/plot.py --input example/output/output_table.tsv --output [targets[0]]", 
+    depends=[args.input],
+    targets=args.output,
     args=[args.lines])
 
 # sample R module task2 
 workflow.add_task(
-    "[depends[0]] --input [depends[1]] -o [targets[0]] -d [args[0]]", 
-    depends=[TrackedExecutable("plot.py"),args.input],
-    targets=[args.output+"example/r_output_table.tsv"])
+    "Rscript src/analysis_example.r -d "+args.sample_metadata+" -o [targets[0]]", 
+    depends=[args.input],
+    targets=[args.output+"/r_output_table.tsv"])
 
 # Generate the pdf report from private analysis 
 document_file = workflow.name_output_files("report.pdf")
 document_vars = {"title":"Demo Analysis Report",
         "project":"Demo Analysis",
-        "introduction_text":"This is a demo report."
+        "introduction_text":"This is a demo report.",
+        "output":"example/output/output_table.tsv"
       }
 
 workflow.add_document(
-    templates=["template.py"],
+    templates=["src/template.py"],
     targets=document_file,
     vars=document_vars)
 
--- src/template.py
+++ src/template.py
@@ -24,3 +24,13 @@ vars = document.get_vars()
 #'
 #'
 #' **Feel free to remove the template code below here and start working on the analysis.** 
+
+#+ echo=False
+column_names,row_names,data=document.read_table(vars["output"])
+
+#' <% print("Total columns: "+str(len(column_names))) %>
+#'
+#' <% print("Total rows: "+str(len(row_names))) %>
+#'
+#+ echo=False
+document.plot_barchart(row_names, labels=column_names, title="Example Visualization Sample", xlabel="rows", ylabel="Abundance")
